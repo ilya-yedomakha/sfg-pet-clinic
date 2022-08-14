4bc1c8a46a1bd82c2baa9fcff5a7de0de850f709
ilya-yedomakha <yedomakhailia@gmail.com>
2022-08-14 21:34:02 +0300

    new commit

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -3,11 +3,13 @@ package guru.springframework.sfgpetclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
+        System.out.println("yes");
     }
 
 }
